28a93c6427a320545ef853fa5369a26ee07d98de
Clark <clarkp@utexas.edu>
2017-04-13 17:55:32 -0500

    Adding 10000 plotting script

---
+++ postproc/figs_10000.py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+# User-defined parameters
+kappa = 0.378
+B = 4.013 
+
+class vars:
+    def __init__(self, filename, Re):
+        self.y, self.U, self.k, self.eps, self.v2, self.f = \
+                np.genfromtxt(filename, usecols=[0,1,2,3,4,5], unpack=True)
+        self.eps /= Re
+        self.yp = np.multiply(self.y, Re)
+        self.Re = Re
+
+    def plot_BL(self, fig, ax):
+        end = 0.35*self.Re #end of the viscous low law region
+        U_powerlaw = self.yp
+        U_loglaw = np.divide(np.log(self.yp),kappa)+B
+        ax.semilogx(self.yp, self.U, 'k', label="$\overline{v^2}-f$ results")
+        ax.semilogx(self.yp, U_powerlaw, 'r:', label="$u^+ = y^+$")
+        ax.semilogx(self.yp, U_loglaw, 'b:', label="$u^+ = \\frac{1}{\kappa} \ln y^+ + B$")
+        ax.set_ylabel('$u^+$')
+        ax.set_xlabel('$y^+$')
+        ax.axis([np.min(self.yp),1.1*end,0,30])
+
+    def plot_U(self, fig, ax, symbol, postfix):
+        ax.semilogx(self.yp, self.U, symbol, label='$u^+$, '+postfix)
+        ax.set_ylabel('$u^+$')
+        ax.set_xlabel('$y^+$')        
+                
+    def plot_k_y(self, fig, ax, symbol, postfix):
+        ax.plot(self.y, self.k, symbol+'--', label='$k$, '+postfix)
+        ax.plot(self.y, self.v2, symbol+'-', label='$\overline{v^2}$, '+postfix)
+        ax.set_ylabel('$k$, $\overline{v^2}$')
+        ax.set_xlabel('$\eta$')
+
+    def plot_k_yp(self, fig, ax, symbol, postfix):
+        ax.semilogx(self.yp, self.k, symbol+'--', label='$k$, '+postfix)
+        ax.semilogx(self.yp, self.v2, symbol+'-', label='$\overline{v^2}$, '+postfix)
+        ax.set_ylabel('$k$, $\overline{v^2}$')
+        ax.set_xlabel('$y^+$')
+
+    def plot_eps(self, fig, ax, symbol, postfix):
+        ax.semilogx(self.yp, self.eps, symbol, label='$\\varepsilon^+$, '+postfix)
+        ax.set_ylabel('$\\varepsilon^+$')
+        ax.set_xlabel('$y^+$')
+
+# Extract the data
+v52 = vars("../data/v2fResults_5200.dat",5200)
+v10 = vars("../data/test/v2fResults_10000.dat",10000)
+
+# Find the log-law parameters
+mask = np.where((v10.yp > 30))
+logy = np.log(v10.yp[mask])
+u_in_log = v10.U[mask]
+slope, intercept, r, p, err  = stats.linregress(logy, u_in_log)
+print("k = {0}".format(1.0/slope))
+print("B = {0}".format(intercept))
+print("r = {0}".format(r))
+
+# Build the plot
+fig, ax = plt.subplots()
+v10.plot_BL(fig, ax)
+ax.legend(loc='best')
+fig.set_size_inches(8,6, forward=True)
+fig.savefig('../doc/turbulence/figs/loglaw_10000.png')
+plt.show()
+
+# Plot k, eps, v2, f
+fig1, ax = plt.subplots()
+v52.plot_U(fig1, ax, 'r', '5200')
+v10.plot_U(fig1, ax, 'b', '10000')
+ax.legend(loc='best')
+fig1.tight_layout()
+fig1.set_size_inches(7,5, forward=True)
+fig1.savefig('../doc/turbulence/figs/U_10000.png')
+plt.show()
+
+fig2, ax = plt.subplots()
+v52.plot_k_y(fig2, ax, 'r', '5200')
+v10.plot_k_y(fig2, ax, 'b', '10000')
+ax.legend(loc='best')
+fig2.tight_layout()
+fig2.set_size_inches(7,5, forward=True)
+fig2.savefig('../doc/turbulence/figs/k_y_10000.png')
+plt.show()
+
+fig3, ax = plt.subplots()
+v52.plot_k_yp(fig2, ax, 'r', '5200')
+v10.plot_k_yp(fig2, ax, 'b', '10000')
+ax.legend(loc='best')
+fig3.tight_layout()
+fig3.set_size_inches(7,5, forward=True)
+fig3.savefig('../doc/turbulence/figs/k_yp_10000.png')
+plt.show()
+
+fig4, ax = plt.subplots()
+v52.plot_eps(fig2, ax, 'r', '5200')
+v10.plot_eps(fig2, ax, 'b', '10000')
+ax.legend(loc='best')
+ax.set_ylim([0,np.max(v10.eps)*1.1])
+fig4.tight_layout()
+fig4.set_size_inches(7,5, forward=True)
+fig4.savefig('../doc/turbulence/figs/eps_10000.png')
+plt.show()
